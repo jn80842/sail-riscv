0f0d940b2ae3cf0de2afff8da650bd987197ddc4
jn80842 <newcomb@galois.com>
2025-06-04 18:47:11 -0700

    add declaration for valid_reservation

--- handwritten_support/RiscvExtrasExecutable.lean
+++ handwritten_support/RiscvExtrasExecutable.lean
@@ -67,6 +67,7 @@ def plat_term_read : Unit → SailM String := λ _ => panic "TODO"
 def load_reservation : Arch.pa → SailM Unit := λ _ => panic "TODO"
 def match_reservation : Arch.pa → Bool := λ _ => panic "TODO"
 def cancel_reservation : Unit → SailM Unit := λ _ => panic "TODO"
+def valid_reservation : Unit → Bool := λ _ => panic "TODO"
 
 def get_16_random_bits : Unit → SailM (BitVec 16) := λ _ => panic "TODO"
 
